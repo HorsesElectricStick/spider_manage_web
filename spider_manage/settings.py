3aa8406ac267cc80535771e9bf5067c548a1bedb
HorsesElectricStick <35952197+HorsesElectricStick@users.noreply.github.com>
2020-06-23 19:59:16 +0800

    设置关闭浏览器后登陆失效

--- spider_manage/settings.py
+++ spider_manage/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SESSION_SAVE_EVERY_REQUEST = False
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
