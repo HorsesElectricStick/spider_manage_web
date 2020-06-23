2893fa1837b58af8e067a234d5f74b90798300e5
HorsesElectricStick <35952197+HorsesElectricStick@users.noreply.github.com>
2020-06-23 16:41:04 +0800

    init

---
+++ myauth/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here.
+
+def 登录(request, info=None):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['用户名'], password=request.POST['密码'])
+        if user is None:
+            return render(request, 'myauth/login.html', {'错误': '用户名/密码错误!', 'username': request.POST['用户名']})
+        else:
+            login(request, user)
+            return redirect('myauth:主页')
+    else:
+        return render(request, 'myauth/login.html', {'错误': 'get请求'})
+
+def 主页(request):
+    return render(request, 'myauth/index.html')
+
+def 退出(request):
+    logout(request)
+    return redirect('myauth:登录页')
