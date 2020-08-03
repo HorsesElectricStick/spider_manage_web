58f3c87e3acf48239b539b97e4e16c03eb94452d
zy <ilovecpla@163.com>
2020-08-03 16:23:00 +0800

    admin注册应用的models方便管理 models增加__str__，规范PEP格式

--- weixin_catpcha/models.py
+++ weixin_catpcha/models.py
@@ -3,15 +3,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class captcha_API(models.Model):
     token = models.CharField(max_length=300)
     token_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.token_time.__str__()
+
 
 class captcha(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     captcha = models.CharField(max_length=10, null=True)
     weixin_id = models.CharField(max_length=100, null=True)
     captcha_time = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.username.__str__()
+
 
 class API_params(models.Model):
     app_id = models.CharField(max_length=50)
