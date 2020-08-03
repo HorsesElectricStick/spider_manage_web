f3d243d08242979692459ce49923c530ab20a450
zy <ilovecpla@163.com>
2020-08-03 13:07:07 +0800

    注册APP路径代码规范化

--- spider_manage/settings.py
+++ spider_manage/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myauth',
-    'weixin_catpcha'
+    'myauth.apps.MyauthConfig',
+    'weixin_catpcha.apps.WeixinCatpchaConfig'
 ]
 
 MIDDLEWARE = [
